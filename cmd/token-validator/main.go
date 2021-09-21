600880a83135a9c61976f486f2d086d71625e872
Marcin Kaminski <markamdev.84@gmail.com>
2021-09-21 20:59:51 +0200

    SIGINT handler added
    GH-3

--- cmd/token-validator/main.go
+++ cmd/token-validator/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"github.com/markamdev/doorkeeper/lifecycle"
 	"github.com/markamdev/doorkeeper/logger"
 )
 
 func main() {
 	logger.LogDebug("authenticator service")
+
+	lifecycle.WaitForSigint()
+	logger.LogDebug("... closing application")
 }
