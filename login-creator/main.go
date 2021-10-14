a6443992f338a51376cbb4621d32c63e13b5a691
Marcin Kaminski <markamdev.84@gmail.com>
2021-10-14 21:12:41 +0200

    /liveness endpoint in login-creator

--- login-creator/main.go
+++ login-creator/main.go
@@ -3,11 +3,32 @@ package main
 import (
 	"login-creator/lifecycle"
 	"login-creator/logger"
+	"login-creator/rest"
+	"os"
+)
+
+const (
+	listenPort = "8091"
 )
 
 func main() {
 	logger.LogDebug("login-creator service")
 
+	logger.LogDebug("Creating server listening at port:", listenPort)
+	srv, err := rest.CreateServer(listenPort)
+
+	if err != nil {
+		logger.LogError("Failed to create server:", err.Error())
+		os.Exit(1)
+	}
+
+	srv.Start()
+	logger.LogDebug("... server launched")
+
 	lifecycle.WaitForSigint()
-	logger.LogDebug("... closing application")
+
+	logger.LogDebug("Stopping server")
+	srv.Stop()
+
+	logger.LogDebug("Closing application")
 }
